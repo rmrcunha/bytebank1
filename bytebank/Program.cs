4199d7079bd887f8246438208af58272eadfa3fe
Renan Cunha <renancdev@gmail.com>
2022-12-08 21:09:19 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/Program.cs
@@ -0,0 +1,20 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia =  15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = "+contaDoAndre.saldo);
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Souza";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.conta = "1010-5";
+contaDaMaria.saldo = 350;
+
+Console.WriteLine("Saldo da conta da Maria =" + contaDaMaria.saldo);
+
+contaDoAndre.Transferir(50, contaDaMaria);
+Console.WriteLine("Saldo do André =" + contaDoAndre.saldo);
+Console.WriteLine("Saldo da Maria =" + contaDoAndre.saldo);
